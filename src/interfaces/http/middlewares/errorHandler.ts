032799c9ae0d62c2f622d0b652a1d1ee0a1df0dc
Abe Garcia <ab.garcia@outlook.com>
2026-03-02 17:44:56 +0100

    remove console log

--- src/interfaces/http/middlewares/errorHandler.ts
+++ src/interfaces/http/middlewares/errorHandler.ts
@@ -2,8 +2,6 @@ import { ErrorRequestHandler } from 'express';
 import multer from 'multer';
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.error(err);
-
     if (err instanceof multer.MulterError) {
         if (err.code === 'LIMIT_FILE_SIZE') {
             res.status(400).json({ error: 'File too large. Maximum size is 5MB.' });
